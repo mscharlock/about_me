81441784d629530c6e5bda89a13f6cf83307fac4
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-05-09 14:34:23 -0700

    fixed double quotes, added an alert

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 'use strict';
 
-var user = prompt("Welcome to Michelle's Website! What is your name?");
-console.log("user: ",user);
+var user = prompt('Welcome to Michelle\'s Website! What is your name?');
+console.log('user: ',user);
+
+alert('Nice to meet you, ' + user +'. To get to know me better, let\'s play a game. I\'ll ask you five questions about me.');
