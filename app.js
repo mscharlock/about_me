256f71936232627757da97bb442e050ca627f488
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-05-14 18:38:03 -0700

    Updated from Codepen CSS and Day3 game changes

--- app.js
+++ app.js
@@ -5,49 +5,72 @@ console.log('user: ',user);
 
 alert('Nice to meet you, ' + user +'. To get to know me better, let\'s play a game. I\'ll ask you five questions about me.');
 
-var michelleBorn = prompt('Was I born in the USA? Answer with y/n, please.').toLowerCase();
-console.log('michelleBorn: ',michelleBorn);
+//Five questions about me
+//Q1
+var birthPlace = function() {
+  var michelleBorn = prompt('Was I born in the USA? Answer with y/n, please.').toLowerCase();
+  console.log('michelleBorn: ',michelleBorn);
 
-if (michelleBorn === 'yes' || michelleBorn === 'y') {
-  alert('Good guess, but no! I was actually born in Germany!');
-} else if (michelleBorn === 'no' || michelleBorn === 'n') {
-  alert('Correct! I was not born in the US. I was actually born in Heidelberg, Germany.');
-} else {
-  alert('I didn\'t understand your answer but I\'ll just tell you: I was born in Germany!');
+  if (michelleBorn === 'yes' || michelleBorn === 'y') {
+    alert('Good guess, but no! I was actually born in Germany!');
+  } else if (michelleBorn === 'no' || michelleBorn === 'n') {
+    alert('Correct! I was not born in the US. I was actually born in Heidelberg, Germany.');
+  } else {
+    alert('I didn\'t understand your answer but I\'ll just tell you: I was born in Germany!');
+  }
+  return michelleBorn;
 }
+birthPlace();
 
-var michelleCollege = prompt('Did Michelle go to Montreal, to McGill University, for her undergraduate degree?').toLowerCase();
-console.log('michelleCollege: ', michelleCollege);
+//Q2
+var college = function(){
+  var michelleCollege = prompt('Did Michelle go to Montreal, to McGill University, for her undergraduate degree?').toLowerCase();
+  console.log('michelleCollege: ', michelleCollege);
 
-if (michelleCollege === 'yes' || michelleCollege === 'y') {
-  alert('Oui oui!');
-} else {
-  alert('I didn\'t quite catch that! But the correct answer was yes!');
+  if (michelleCollege === 'yes' || michelleCollege === 'y') {
+    alert('Oui oui!');
+  } else {
+    alert('I didn\'t quite catch that! But the correct answer was yes!');
+  }
+  return michelleCollege;
 }
+college();
 
-var michelleLikesCoding = prompt('Do you think Michelle enjoys coding?').toLowerCase();
-console.log('michelleLikesCoding: ', michelleLikesCoding);
+//Q3
+var coding = function() {
+  var michelleLikesCoding = prompt('Do you think Michelle enjoys coding?').toLowerCase();
+  console.log('michelleLikesCoding: ', michelleLikesCoding);
 
-if (michelleLikesCoding === 'y' || michelleLikesCoding === 'yes') {
-  alert('Right!!');
+  if (michelleLikesCoding === 'y' || michelleLikesCoding === 'yes') {
+    alert('Right!!');
+  }
+  else {
+    alert('That was an easy one! Of course she likes coding!');
+  }
+  return michelleLikesCoding;
 }
-else {
-  alert('That was an easy one! Of course she likes coding!');
-}
+coding();
 
-var michelleHasPets = prompt ('Do you think Michelle has any pets?').toLowerCase();
-console.log('michelleHasPets: ', michelleHasPets);
+//Q4
+var pets = function() {
+  var michelleHasPets = prompt ('Do you think Michelle has any pets?').toLowerCase();
+  console.log('michelleHasPets: ', michelleHasPets);
 
-if (michelleHasPets === 'y' || michelleHasPets === 'yes') {
-  alert('Yes! She has a very fuzzy cat named Toby');
+  if (michelleHasPets === 'y' || michelleHasPets === 'yes') {
+    alert('Yes! She has a very fuzzy cat named Toby');
+  }
+  else if (michelleHasPets === 'cat' ||
+  michelleHasPets === 'cats') {
+    alert('Yes! She does have a cat.');
+  } else {
+    alert('Wrong! She has a cat!');
+  }
+  return michelleHasPets;
 }
-else if (michelleHasPets === 'cat' ||
-michelleHasPets === 'cats') {
-  alert('Yes! She does have a cat.');
-} else {
-  alert('Wrong! She has a cat!');
-}
+pets();
 
+//Q5
+var whoWantsToLiveForever = function() {
 var michelleImmortality = prompt('If Michelle could choose to be immortal, would she do it?').toUpperCase();
 console.log('michelleImmortality: ' , michelleImmortality);
 
@@ -58,100 +81,58 @@ if (michelleImmortality === 'YES' || michelleImmortality === 'Y') {
 } else {
   alert('You didn\'t answer yes or no! Pffffttttt!');
 }
-
-// var michelleFictionalCharacterBff = prompt('Time for one last question! If Michelle could be best friends with any fictional character, who would she choose to befriend?');
-// console.log('michelleFictionalCharacterBff: ', michelleFictionalCharacterBff);
-//
-// if (michelleFictionalCharacterBff === "Sherlock Holmes" || michelleFictionalCharacterBff === "Sherlock") {
-//   alert('Bingo, super sleuth! The game is afoot!')
-// }
-// else if (michelleFictionalCharacterBff === "Benedict Cumberbatch") {
-//   alert("Eh, close enough! We'll mark that as right!")
-// }
-// else if (michelleFictionalCharacterBff === 'Mr. Darcy') {
-//   alert('Uhhhh no. He\'s way too whiny!')
-// }
-// else if (michelleFictionalCharacterBff === 'Buffy' || michelleFictionalCharacterBff === 'Batman' || michelleFictionalCharacterBff === 'Superman') {
-//   alert('Those are pretty popular characters, but nope! The answer was Sherlock Holmes!')
-// } else {
-//   alert('Good guess! But the answer was Sherlock Holmes. Sherlock...it\'s pretty close to Scharlock, right?')
-// }
+return michelleImmortality;
+}
+whoWantsToLiveForever();
 
 //Six tries to guess one of multiple answers
-var ficCharas = ['Sherlock Holmes', 'Catwoman', 'Fluttershy', 'Xena'];
+var ficCharas = ['sherlock holmes', 'sherlock','catwoman', 'fluttershy', 'xena'];
 var flag;
-var michelleFictionalCharacterBff = prompt('What fictional character would Michelle hang out with?');
-console.log('michelleFictionalCharacterBff: ', michelleFictionalCharacterBff);
 
-var guessCounter = 0;
-console.log('guessCounter: ', guessCounter);
-
-while (guessCounter <= 6) {
-  for (var i=0; i < ficCharas.length; i++) {
-    console.log('current guess: ', michelleFictionalCharacterBff);
-
-    if (michelleFictionalCharacterBff === ficCharas[i]) {
-      alert('Yay! You guessed right! All of the possible answers were: ' + ficCharas[i]);
-      flag = true;
-      break;
-    }
-    else if (!flag) {
-      alert('No guess again!');
-      guessCounter++;
-
-    }
-  }
+for (i = 0; i <= 5; i++) {
+  var charaGuess = prompt('What fictional character would Michelle hang out with?').toLowerCase;
+  if (charaGuess === ficCharas[i]) {
+    alert('Yes indeed! She would definitely hang out with them!');
+    flag = true;
+    break;
 }
+   if (!flag) {
+  alert('Hrmmm...guess again');
+}
+  if (i === 5 && charaGuess != ficCharas[i]) {
+    ('Sorry, the answers were: Sherlock Holmes, Catwoman, Fluttershy, and/or Xena!');
+  }
 
 //Guess a number
-var myNumber;
-var counter = 0;
+var numberGuess = function() {
+  var myNumber;
+  var counter = 0;
 
-while (myNumber !== 12) {
-  myNumber = parseInt(prompt('What is my favorite number? Take a guess!'));
-
-  if (myNumber > 50){
-    alert('Too high!');
-    counter++;
-  } else if (myNumber < 3) {
-    alert('Too low!');
-    counter++;
-  } else if (myNumber < 8 && myNumber > 20) {
-    alert ('You\'re getting closer...');
-    counter++;
-  } else if (myNumber === 12) {
-    alert('Yay, you win!');
-  }
-
-  if (counter === 4) {
-    alert('Sorry, you ran out of tries - my favorite number is 12!');
-    break;
+  while (myNumber !== 12 && counter < 4) {
+    myNumber = parseInt(prompt('What is my favorite number? Take a guess!'));
+    console.log(myNumber);
+    if (myNumber >=13){
+      alert('Too high!');
+      counter++;
+      console.log('counter' + counter);
+    } else if (myNumber <= 11) {
+      alert('Too low!');
+      counter++;
+      console.log('counter' + counter);
+    } else if (isNaN(myNumber)) {
+      alert ('Enter a number please...');
+      counter++;
+      console.log('counter' + counter);
+    } else if (myNumber === 12) {
+      alert('Yay, you win!');
+      console.log('counter' + counter);
+      // return myNumber;
+    } else if (counter > 4) {
+      alert('Sorry, you ran out of tries - my favorite number is 12!');
+      return myNumber;
+    }
   }
 }
 
 console.log('counter: ', counter);
-
-
-//guessing that stupid fictional chara thing again
-// var flag;
-// //console log
-// var michellesArray = ['Cat', 'Dog'];
-// //console log
-// var guessCount = 0;
-// //console log
-// var yourGuess = prompt('What pet');
-// //console log
-//
-// while (guessCount <= 6) {
-//   while (yourGuess != michellesArray[i]) {
-//     alert('NO');
-//     guessCount++;
-//     console.log(guessCount);
-//
-//     if (yourGuess === michellesArray[i]){
-//       alert('YAY');
-//       guessCount++;
-//       console.log(guessCount);
-//     }
-//   }
-// }
+numberGuess();
